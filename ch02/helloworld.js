d14bdcc8b7146fbf062001f4fb73d25e14d092bc
wonjoon <zzzdnjswns@naver.com>
2022-10-01 01:15:53 +0900

    static file

--- ch02/helloworld.js
+++ ch02/helloworld.js
@@ -2,8 +2,22 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World");
+  // 쿼리스트링, 옵션인 마지막 슬래시를 없애고 소문자로 바꿔서 url을 졍규화 합니다.
+  const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+  switch (path) {
+    case "":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Homepage");
+      break;
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text.plain" });
+      res.end("About");
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Not Fount");
+      break;
+  }
 });
 
 server.listen(port, () =>
