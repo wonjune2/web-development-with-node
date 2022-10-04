c23704c5a568c77074f2b0e988f62caebbee2d4a
wonjoon <zzzdnjswns@naver.com>
2022-10-05 04:55:47 +0900

    eslint

--- ch05/lib/handlers.js
+++ ch05/lib/handlers.js
@@ -6,5 +6,6 @@ exports.about = (req, res) =>
   res.render("about", { fortune: fortune.getFortune() });
 
 exports.notFount = (req, res) => res.render("404");
-
+/* eslint-disable no-unused-vars */
 exports.serverError = (err, req, res, next) => res.render("500");
+/* eslint-disable no-unused-vars */
